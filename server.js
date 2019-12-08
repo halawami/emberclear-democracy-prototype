38091c9ce6e508a6f18f78a67ab3f43fd0ebdd84
Austin Gassert <amgassert@gmail.com>
2019-12-08 13:50:02 -0500

    Add server boilerplate

---
+++ server.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js");
+const endpoints = require("./endpoints.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.on("ready", () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("message", async message => {
+    endpoints.handleMessage(message);
+});
+
+client.login(config.token);
