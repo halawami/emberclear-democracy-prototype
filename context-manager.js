d6a526898dee68c40fa921c1876202ac3a5d5fb2
CelticMajora <cdonoho16@gmail.com>
2019-12-08 13:47:11 -0500

    Tried converting to a different syntax form. Will need to ask Austin where he found the info on that style

--- context-manager.js
+++ context-manager.js
@@ -1,55 +1,62 @@
-var contexts = [];
+let ContextManager = {
 
-function add_channel(channel_uid, user_uid){
-	if(contexts.every((context) => context.channel !== channel_uid)){
-		contexts.push({
-			"channel": channel_uid,
-			"user_contexts": [{
-				"user": user_uid,
-		        "user_context": {
-		        	"admin": user_uid,
-		        	"members": [
-		            	user_uid
-		          	]
-		        }
-			}]
-		})
-	}	
-}
-
-function add_user(channel_uid, user_uid){
-	var channelContext = contexts.find((context) => context.channel === channel_uid)
-	if(channelContext !== undefined && channelContext.user_contexts.every((userContext) => userContext.user !== user_uid)){
-		channelContext.user_contexts.push({
-			"user": user_uid,
-			"user_context": {
-				"admin": "FILL THESE TWO IN USING DEMOCRACY ALGO",
-				"members": []
-			}
-		})
+	constructor(){
+		this.contexts = []
 	}
-}
 
-function delete_user(channel_uid, user_uid){
-	var channelContext = contexts.find((context) => context.channel === channel_uid)
-	if(channelContext !== undefined){
-		var index = channelContext.user_contexts.findIndex((userContext) => userContext.user === user_uid)
-		if(index !== -1){
-			channelContext.user_contexts.splice(index, 1)
-			channelContext.user_contexts.forEach((userContext) => {
-				var userMemberIndex = userContext.user_context.members.findIndex((member) => member === user_uid)
-				if(userMemberIndex !== -1){
-					userContext.user_context.members.splice(userMemberIndex, 1)
+	add_channel: function(channel_uid, user_uid){
+		if(this.contexts.every((context) => context.channel !== channel_uid)){
+			this.contexts.push({
+				"channel": channel_uid,
+				"user_contexts": [{
+					"user": user_uid,
+			        "user_context": {
+			        	"admin": user_uid,
+			        	"members": [
+			            	user_uid
+			          	]
+			        }
+				}]
+			})
+		}	
+	}
+
+	add_user: function(channel_uid, user_uid){
+		var channelContext = this.contexts.find((context) => context.channel === channel_uid)
+		if(channelContext !== undefined && channelContext.user_contexts.every((userContext) => userContext.user !== user_uid)){
+			channelContext.user_contexts.push({
+				"user": user_uid,
+				"user_context": {
+					"admin": "FILL THESE TWO IN USING DEMOCRACY ALGO",
+					"members": []
 				}
 			})
 		}
 	}
-}
 
-function change_admin(channel_uid, user_uid){
-	var channelContext = contexts.find((context) => context.channel === channel_uid)
-	if(channelContext !== undefined){
-		// ADD CHECK TO SEE IF user_uid IS ACTUALLY IN THE CHANNEL
-		channelContext.user_contexts.forEach((userContext) => userContext.user_context.admin = user_uid)
+	delete_user: function(channel_uid, user_uid){
+		var channelContext = this.contexts.find((context) => context.channel === channel_uid)
+		if(channelContext !== undefined){
+			var index = channelContext.user_contexts.findIndex((userContext) => userContext.user === user_uid)
+			if(index !== -1){
+				channelContext.user_contexts.splice(index, 1)
+				channelContext.user_contexts.forEach((userContext) => {
+					var userMemberIndex = userContext.user_context.members.findIndex((member) => member === user_uid)
+					if(userMemberIndex !== -1){
+						userContext.user_context.members.splice(userMemberIndex, 1)
+					}
+				})
+			}
+		}
+	}
+
+	change_admin: function(channel_uid, user_uid){
+		var channelContext = this.contexts.find((context) => context.channel === channel_uid)
+		if(channelContext !== undefined){
+			// ADD CHECK TO SEE IF user_uid IS ACTUALLY IN THE CHANNEL
+			channelContext.user_contexts.forEach((userContext) => userContext.user_context.admin = user_uid)
+		}
 	}
 }
+
+export default ContextManager
