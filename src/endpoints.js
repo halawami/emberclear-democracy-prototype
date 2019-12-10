9b8700a66c5da0bc5aa5b9c787ffdb4b840a6080
Hussein Alawami <alawamhm@rose-hulman.edu>
2019-12-10 01:00:19 -0500

    add/remove roles logic

--- src/endpoints.js
+++ src/endpoints.js
@@ -16,8 +16,40 @@ function messageHandler(message){
 		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
 		if(!member)
 		  return message.reply("Please mention a valid member of this server");
-			// TODO: call democracy function
+		// TODO: call democracy function
 	}	
+
+	if (command === "add-role") {
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+		if(!member)
+		  return message.reply("Please mention a valid member of this server");
+		
+		let role = message.guild.roles.find(role => role.name === args[1]);
+		if (!role)
+	    	return console.log("The role does not exist");
+		
+		if (member.roles.has(role.id))
+			return message.reply("Member already has the " + role.name + " role.");
+
+		member.addRole(role).catch(console.error);
+		return message.reply(role.name + " added to " + member);
+	}
+
+	if (command === "remove-role") {
+		let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+		if(!member)
+		  return message.reply("Please mention a valid member of this server");
+		
+		let role = message.guild.roles.find(role => role.name === args[1]);
+		if (!role)
+	    	return console.log("The role does not exist");
+		
+		if (!member.roles.has(role.id))
+			return message.reply("Member doesn't have the " + role.name + " role.");
+
+		member.removeRole(role).catch(console.error);
+		return message.reply(role.name + " removed from " + member);
+	}
 
 	if (command === "add-member") {
 		// TODO: call democracy function
